2f38ab88480ab9ce8e90537c914390c9f8c11c6b
Alexandre M. S. Lima <alexandre.lima@comexport.com.br>
2019-07-16 09:28:05 -0300

    Refactoring

--- src/main/java/br/com/cafebinario/logger/LogInterceptor.java
+++ src/main/java/br/com/cafebinario/logger/LogInterceptor.java
@@ -14,51 +14,84 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogInterceptor {
 
-	@Around("@annotation(Log)")
-	public Object log(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
-		final Long begin = System.currentTimeMillis();
-		final MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
-		final Method method = methodSignature.getMethod();
-		final String methodName = methodSignature.getName();
-		final String[] parameterNames = methodSignature.getParameterNames();
-		final Class<?>[] parameterTypes = methodSignature.getParameterTypes();
-		final Class<?> returnType = methodSignature.getReturnType();
-		
-		final Log logAnnotation = method.getDeclaredAnnotation(Log.class);
-		
-		final VerboseMode verboseMode = logAnnotation.verboseMode();
-		
-		final LogContext logContext = LogContext
-				.builder()
-				.methodName(methodName)
-				.parameterNames(parameterNames)
-				.parameterTypes(parameterTypes)
-				.parameterValues(proceedingJoinPoint.getArgs())
-				.returnType(returnType)
-				.build();
+    @Around("@annotation(Log)")
+    public Object log(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 
-		final Logger logger = LoggerFactory.getLogger(proceedingJoinPoint.getSignature().getDeclaringType());
-		
-		try {
-			
-			verboseMode.logParameters(logContext, logger);
-			
-			final Object returnValue = proceedingJoinPoint.proceed();
-			
-			verboseMode.logReturn(logContext
-					.toBuilder()
-					.returnValue(returnValue)
-					.enlapseTime(System.currentTimeMillis() - begin)
-					.build(), logger, returnValue);
+        final Long begin = System.currentTimeMillis();
 
-			return returnValue;
-			
-		}catch (Throwable t) {
-			verboseMode.logException(logContext
-					.toBuilder()
-					.enlapseTime(System.currentTimeMillis() - begin)
-					.build(), logger, t);
-			throw t;
-		}
-	}
+        final Class<?> targetClass = proceedingJoinPoint.getSignature().getDeclaringType();
+        
+        final MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint
+                .getSignature();
+        
+        final Object[] args = proceedingJoinPoint.getArgs();
+
+        final LogContext logContext = createLogContext(methodSignature, args);
+
+        final VerboseMode verboseMode = getVerboseMode(methodSignature);
+
+        final Logger logger = LoggerFactory.getLogger(targetClass);
+
+        try {
+
+            return proccessLog(proceedingJoinPoint, begin, logContext, verboseMode, logger);
+
+        } catch (Throwable t) {
+
+            verboseMode.logException(
+                    logContext
+                    .toBuilder()
+                    .enlapseTime(System.currentTimeMillis() - begin)
+                    .build(),
+                    logger, t);
+
+            throw t;
+        }
+    }
+
+    private LogContext createLogContext(final MethodSignature methodSignature,
+            final Object[] args) {
+
+        final String methodName = methodSignature.getName();
+        final String[] parameterNames = methodSignature.getParameterNames();
+        final Class<?>[] parameterTypes = methodSignature.getParameterTypes();
+        final Class<?> returnType = methodSignature.getReturnType();
+
+        return LogContext
+                .builder()
+                .methodName(methodName)
+                .parameterNames(parameterNames)
+                .parameterTypes(parameterTypes)
+                .parameterValues(args)
+                .returnType(returnType)
+                .build();
+    }
+
+    private Object proccessLog(final ProceedingJoinPoint proceedingJoinPoint, final Long begin,
+            final LogContext logContext, final VerboseMode verboseMode, final Logger logger)
+            throws Throwable {
+
+        verboseMode.logParameters(logContext, logger);
+
+        final Object returnValue = proceedingJoinPoint.proceed();
+
+        verboseMode.logReturn(
+                logContext
+                    .toBuilder()
+                    .returnValue(returnValue)
+                        .enlapseTime(System.currentTimeMillis() - begin)
+                        .build(),
+                logger, returnValue);
+
+        return returnValue;
+    }
+
+    private VerboseMode getVerboseMode(final MethodSignature methodSignature) {
+
+        final Method method = methodSignature.getMethod();
+
+        final Log logAnnotation = method.getDeclaredAnnotation(Log.class);
+
+        return logAnnotation.verboseMode();
+    }
 }
