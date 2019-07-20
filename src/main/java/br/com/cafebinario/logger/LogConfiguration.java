b86e6a22210d00cdbb2744a315b836d82ad0a703
alexandre <alexandre.msl@gmail.com>
2019-07-20 01:49:25 -0300

    add Configuration

---
+++ src/main/java/br/com/cafebinario/logger/LogConfiguration.java
@@ -0,0 +1,10 @@
+package br.com.cafebinario.logger;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LogConfiguration {
+
+}
