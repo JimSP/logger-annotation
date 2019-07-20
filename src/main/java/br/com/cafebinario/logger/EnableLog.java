b22a4f7462b0f86a88e98389e4a0b7c857659404
alexandre <alexandre.msl@gmail.com>
2019-07-20 02:41:42 -0300

    EnableLog annotation

---
+++ src/main/java/br/com/cafebinario/logger/EnableLog.java
@@ -0,0 +1,15 @@
+package br.com.cafebinario.logger;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Import;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import(value = { LogConfiguration.class })
+public @interface EnableLog {
+
+}
--- src/main/java/br/com/cafebinario/logger/LogConfiguration.java
+++ src/main/java/br/com/cafebinario/logger/LogConfiguration.java
@@ -1,10 +1,20 @@
 package br.com.cafebinario.logger;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.EnableLoadTimeWeaving;
+import org.springframework.context.annotation.EnableLoadTimeWeaving.AspectJWeaving;
+import org.springframework.context.annotation.LoadTimeWeavingConfigurer;
+import org.springframework.instrument.classloading.LoadTimeWeaver;
+import org.springframework.instrument.classloading.ReflectiveLoadTimeWeaver;
 
 @Configuration
-@EnableAspectJAutoProxy
-public class LogConfiguration {
+@EnableLoadTimeWeaving(aspectjWeaving = AspectJWeaving.ENABLED)
+public class LogConfiguration implements LoadTimeWeavingConfigurer {
+
+	@Override
+	public LoadTimeWeaver getLoadTimeWeaver() {
+
+		return new ReflectiveLoadTimeWeaver();
+	}
 
 }
